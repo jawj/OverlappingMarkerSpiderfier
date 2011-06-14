d7cb14969ff6d5b3a95be57fd5a2d52c4718cbeb
George MacKerron <gjm06@37.26.dyn.lse.ac.uk>
2011-06-14 18:45:15 +0100

    Tweaked build system

--- build/build.sh
+++ build/build.sh
@@ -7,15 +7,16 @@ OUTDIR=./tmp/
 OUTNAME=${INPREFIX}.min.js
 OUTFILE=${OUTDIR}${OUTNAME}
 
-TEMPLATE=$(cat template.js)
-
 coffee --output $OUTDIR --compile ${INDIR}${INPREFIX}.coffee
+
+echo '/* Built:' $(date) '*/' > $OUTFILE
+cat template.js >> $OUTFILE
 
 java -jar ~/bin/closure-compiler.jar \
   --compilation_level ADVANCED_OPTIMIZATIONS \
   --js ${OUTDIR}${INPREFIX}.js \
   --externs google_maps_api_v3_5.js \
-  --output_wrapper "${TEMPLATE}" \
-> $OUTFILE
+  --output_wrapper '(function(){%output%}).call(this);' \
+>> $OUTFILE
 
 cp $OUTFILE ../../gh-pages/bin
--- build/template.js
+++ build/template.js
@@ -5,4 +5,3 @@
  * Copyright (c) 2011 George MacKerron
  * Released under the MIT licence: http://opensource.org/licenses/mit-license
  */
-(function(){%output%}).call(this);
